085d96ba8257a861378b46b38ac7ea08338ff4d6
Shoyab_Syed <syedshoyab2002@gmail.com>
2025-07-09 15:22:33 +0530

    adding third test docker file using react

---
+++ 03.test-docker-file/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>íº€ Hello from Dockerized React App!</h1>
+      <p>í´¥ You successfully built a React app with multi-stage Dockerfile.</p>
+    </div>
+  );
+}
+
+export default App;
+
