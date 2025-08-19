ba5c094b8f65bdac2f4993d02387c801e16c3a24
Shoyab_Syed <syedshoyab2002@gmail.com>
2025-08-19 17:06:25 +0530

    practice

--- learning_terraform/1.main.tf
+++ learning_terraform/1.main.tf
@@ -15,6 +15,30 @@ resource "aws_instance" "test" {
         Name = "TF demo1"
     }
 }
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-security-group"
+  description = "Allow HTTP traffic"
+  vpc_id      = var.vpc_id 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
 
 variable "ami" {
     description = "our instance ami"
